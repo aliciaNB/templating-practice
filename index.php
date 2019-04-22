4a724e01397890fe0115fb9ad9faa00ff837da22
Alicia Buehner <alicia.buehner@gmail.com>
2019-04-22 16:14:05 -0400

    Add default route and homepage

--- index.php
+++ index.php
@@ -12,8 +12,8 @@ $f3 = Base::instance();
 
 //Define a default route
 $f3->route('GET /', function() {
-//    $view = new Template();
-    echo "<h1>Templating Practice</h1>";
+    $view = new Template();
+    echo $view-> render('views/home.html');
 });
 
 //Run Fat-free
